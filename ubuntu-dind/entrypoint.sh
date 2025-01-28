8e98ea026f34eb7210aa4025f1e6112918d3953e
Till Spindler <14042276+spindev@users.noreply.github.com>
2025-01-28 14:15:19 +0100

    chore: fix for entrypoint.sh

--- ubuntu-dind/entrypoint.sh
+++ ubuntu-dind/entrypoint.sh
@@ -1,41 +1,23 @@
 #!/bin/bash
 source /opt/bash-utils/logger.sh
 
-DOTENV_FILE=/actions-runner/.env
-
-configure_runner() {
-    cd /actions-runner || exit
-
-    INFO "Allowing runner to run as root..."
-    export RUNNER_ALLOW_RUNASROOT="1"
-
-    DATA_DIR="/actions-runner/data/$HOSTNAME"
-
-    if [ -d "$DATA_DIR" ]; then
-        INFO "Runner already exists, copying configuration files..."
-        cp "$DATA_DIR/.credentials" "$DATA_DIR/.runner" "$DATA_DIR/.credentials_rsaparams" /actions-runner/
+load_env_variables() {
+    DOTENV_FILE=/actions-runner/.env
+    INFO "Loading environment variables from ${DOTENV_FILE} file..."
+    if [ -f ${DOTENV_FILE} ]; then
+        while IFS='=' read -r key value; do
+            # Skip comments and empty lines
+            if [[ ! $key =~ ^# ]] && [[ -n $key ]]; then
+                export "$key"="$value"
+            fi
+        done < ${DOTENV_FILE}
     else
-        INFO "Creating runner data directory..."
-        mkdir -p "$DATA_DIR"
-
-        INFO "Runner does not exist, configuring..."
-        ./config.sh \
-            --url ${RUNNER_GITHUB_URL}/${RUNNER_ORG} \
-            --token ${RUNNER_REG_TOKEN} \
-            --name ${HOSTNAME} \
-            --runnergroup ${RUNNER_GROUP} \
-            --labels ${RUNNER_LABELS} \
-            --unattended
-
-        INFO "Backing up the configuration files..."
-        cp /actions-runner/.credentials /actions-runner/.credentials_rsaparams /actions-runner/.runner "$DATA_DIR"
+        INFO "${DOTENV_FILE} file not found!"
+        exit 1
     fi
 }
 
-start_runner() {
-    INFO "Starting the runner..."
-    ./run.sh --once
-}
+load_env_variables
 
-configure_runner
-start_runner
+start-docker.sh
+start-runner.sh
